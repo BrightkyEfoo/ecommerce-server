0df8f564916a36ca9e1775d225d2f22da9267f13
Bright Efoo <brightefoo@gmail.com>
2024-04-14 22:45:50 +0200

    Added tests for the `verifyRoles` function

---
+++ tests/utils/verifyRoles.test.ts
@@ -0,0 +1,64 @@
+import { expect, it, describe } from 'vitest';
+import { verifyRoles } from '../../src/utils/verifyRoles';
+import { TPermission } from '../../src/constants/Roles';
+
+describe('verify roles', () => {
+    it('It sould return true if roles matches', () => {
+        const userRoles = [1, 2, 3];
+        const wantedRoles: TPermission[] = [{
+            obj: 'products',
+            permissions: ['create'],
+        }, {
+            obj: 'categories',
+            permissions: ['create'],
+        }];
+        const res = verifyRoles(userRoles, wantedRoles);
+        expect(res).toBe(true);
+    });
+
+    it('It sould return false if roles doesnt matches', () => {
+        const userRoles = [1];
+        const wantedRoles: TPermission[] = [{
+            obj: 'products',
+            permissions: ['create'],
+        }];
+        const res = verifyRoles(userRoles, wantedRoles);
+        expect(res).toBe(false);
+    });
+
+    it('It sould return true if empty roles', () => {
+        const userRoles = [];
+        const wantedRoles: TPermission[] = [{
+            obj: 'products',
+            permissions: ['create'],
+        }];
+        const res = verifyRoles(userRoles, wantedRoles);
+        expect(res).toBe(false);
+    });
+
+    it('It sould return true if diferent roles provided matches', () => {
+        const userRoles = [1, 2];
+        const wantedRoles: TPermission[] = [{
+            obj: 'products',
+            permissions: ['create'],
+        }, {
+            obj: 'users',
+            permissions: ['create'],
+        }];
+        const res = verifyRoles(userRoles, wantedRoles);
+        expect(res).toBe(true);
+    });
+
+    it('It sould return false if diferent roles provided doesnt matches', () => {
+        const userRoles = [1];
+        const wantedRoles: TPermission[] = [{
+            obj: 'products',
+            permissions: ['create'],
+        }, {
+            obj: 'users',
+            permissions: ['create'],
+        }];
+        const res = verifyRoles(userRoles, wantedRoles);
+        expect(res).toBe(false);
+    });
+});
