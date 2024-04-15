9675d015c08e012d7583c16ac76572c0bea757c9
Bright Efoo <brightefoo@gmail.com>
2024-04-15 07:26:08 +0200

    update connect method

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,10 +2,15 @@ import mongoose from 'mongoose';
 
 export const dbInit = async () => {
     try {
-        if (!process.env.DB_URI) {
+        if (!process.env.DB_URI || !process.env.DB_USER || !process.env.DB_PASSWORD) {
             return false;
         }
-        await mongoose.connect(process.env.DB_URI);
+        await mongoose.connect(process.env.DB_URI, {
+            auth: {
+                username: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+            },
+        });
         return true;
     } catch (error: any) {
         console.error('error connecting Db', error);
